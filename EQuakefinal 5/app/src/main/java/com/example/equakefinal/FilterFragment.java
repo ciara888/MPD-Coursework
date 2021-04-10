ad67398a0376c85c87006f3136a7610028852891
ciara888 <72667822+ciara888@users.noreply.github.com>
2021-04-10 13:08:14 +0100

    Add files via upload

---
+++ EQuakefinal 5/app/src/main/java/com/example/equakefinal/FilterFragment.java
@@ -0,0 +1,22 @@
+package com.example.equakefinal;
+
+
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to create an instance of this fragment.
+ */
+public class FilterFragment extends Fragment {
+
+
+
+
+
+
+
+
+
+}
